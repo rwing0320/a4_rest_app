f36883fbfa27b9c84a605f6505337174b9c98f6e
ryan wing <ryan_645@outlook.com>
2019-11-28 17:49:33 -0500

    Added another api call to get random cocktail. Ryan W - 15 minutes

--- A4_Rest_App/WordsController.cs
+++ A4_Rest_App/WordsController.cs
@@ -53,72 +53,8 @@ namespace A4_Rest_App
 
                 if (values.ContainsKey("drinks"))
                 {
-                    
-                    foreach(var item in values)
-                    {
-                       
-                        IEnumerable list = item.Value as IEnumerable;
-                                       
-
-                        foreach (Object element in list)
-                        {                        
-
-                            Dictionary<string, string> newVal = JsonConvert.DeserializeObject<Dictionary<string, string>>(element.ToString());
-
-                            Dictionary<string, string> keyValuePairDict = new Dictionary<string, string>();
-
-                            for (var i = 0; i < keyLookingFor.Length; i++)
-                            {
-                                if (i < 7)
-                                {
-
-                                    if (newVal.ContainsKey(keyLookingFor[i]))
-                                    {
-                                        if (newVal[keyLookingFor[i]] != null && newVal[keyLookingFor[i]] != "")
-                                        {
-                                            keyValuePairDict.Add(keyLookingFor[i], newVal[keyLookingFor[i]]);
-
-                                            Console.WriteLine("THE ELEMENT IS: " + newVal[keyLookingFor[i]]);
-                                        }
-                                    }
-                                }
-                                else
-                                {
-                                    int newCount = 1;
-                                  
-
-                                    bool isNotFound = false;
-                                    do
-                                    {
-                                        string keyName = keyLookingFor[i] + newCount;
-                                        if (newVal.ContainsKey(keyName))
-                                        {
-                                            if (newVal[keyName] != null && newVal[keyName] != "")
-                                            {
-                                                keyValuePairDict.Add(keyName, newVal[keyName]);
-                                                newCount++;
-                                                Console.WriteLine("THE ELEMENT IS: " + newVal[keyName]);
-                                            }
-                                            else
-                                            {
-                                                isNotFound = true;
-                                            }
-                                        }
-                                        else
-                                        {
-                                            isNotFound = true;
-                                        }
-                                    } while (isNotFound == false);
-
-                                }
-                            }
-
-                            newDictionary.Add(counter, keyValuePairDict);
-                            counter++;
-                        }
-
-                    }
-                    return JsonConvert.SerializeObject(newDictionary);
+                   string myDrink =  getDrinkInfo(values);
+                    return myDrink;
 
                 }
                 else
@@ -142,6 +78,118 @@ namespace A4_Rest_App
 
             return "";
 
+        }
+
+        [HttpGet]
+        [Route("/api/drink/random")]
+        [Obsolete]
+        public string getRandomCocktail(){
+            var user = HttpContext.User;
+            Boolean isSignedIn = user.Identity.IsAuthenticated;
+
+            if (isSignedIn == true)
+            {
+                //drinkName = drinkName.ToLower();
+                //var client = new RestClient("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita");
+                string url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+                var client = new RestClient(url);
+
+                var request = new RestRequest(Method.GET);
+                //request.AddHeader("x-rapidapi-host", "wordsapiv1.p.rapidapi.com");
+                //request.AddHeader("key", "1");
+                IRestResponse response = client.Execute(request);
+
+                string data = response.Content;
+
+                Dictionary<string, object> values = JsonConvert.DeserializeObject<Dictionary<string, object>>(data);
+
+                string drinkInfo = getDrinkInfo(values);
+
+                //return  JsonConvert.SerializeObject(values);
+                return drinkInfo;
+             }
+             return "";
+        
+        }
+
+        public string getDrinkInfo(Dictionary<string, object> values)
+        {
+              string[] keyLookingFor = { "idDrink", "strDrink", "strCategory", "strAlcoholic","strGlass", "strInstructions", "strDrinkThumb", "strIngredient", "strMeasure"};
+
+            // Dictionary<string, Dictionary<string, string>[]> newDictionary = new Dictionary<string, Dictionary<string, string>[]>();
+
+            Dictionary<int, Dictionary<string, string>> newDictionary = new Dictionary<int, Dictionary<string, string>>();
+            int counter = 1;
+          
+
+                foreach (var item in values)
+                {
+
+                    IEnumerable list = item.Value as IEnumerable;
+
+
+                    foreach (Object element in list)
+                    {
+
+                        Dictionary<string, string> newVal = JsonConvert.DeserializeObject<Dictionary<string, string>>(element.ToString());
+
+                        Dictionary<string, string> keyValuePairDict = new Dictionary<string, string>();
+
+                        for (var i = 0; i < keyLookingFor.Length; i++)
+                        {
+                            if (i < 7)
+                            {
+
+                                if (newVal.ContainsKey(keyLookingFor[i]))
+                                {
+                                    if (newVal[keyLookingFor[i]] != null && newVal[keyLookingFor[i]] != "")
+                                    {
+                                        keyValuePairDict.Add(keyLookingFor[i], newVal[keyLookingFor[i]]);
+
+                                        Console.WriteLine("THE ELEMENT IS: " + newVal[keyLookingFor[i]]);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                int newCount = 1;
+
+
+                                bool isNotFound = false;
+                                do
+                                {
+                                    string keyName = keyLookingFor[i] + newCount;
+                                    if (newVal.ContainsKey(keyName))
+                                    {
+                                        if (newVal[keyName] != null && newVal[keyName] != "")
+                                        {
+                                            keyValuePairDict.Add(keyName, newVal[keyName]);
+                                            newCount++;
+                                            Console.WriteLine("THE ELEMENT IS: " + newVal[keyName]);
+                                        }
+                                        else
+                                        {
+                                            isNotFound = true;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        isNotFound = true;
+                                    }
+                                } while (isNotFound == false);
+
+                            }
+                        }
+
+                        newDictionary.Add(counter, keyValuePairDict);
+                        counter++;
+                    }
+
+                }
+                return JsonConvert.SerializeObject(newDictionary);
+
+           
         }
     }
 }
